39a4113b648b1f2bc0b5db987e8b1ccb7a8a89e0
Jayon Kim <jpkim921@gmail.com>
2021-12-14 17:18:16 -0500

    start moralis SDK

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ import chalk from 'chalk'
 import clear from 'clear'
 import figlet from 'figlet'
 
-// const server = require("./lib/node.js");
-import {server} from "./lib/node.js";
 
 clear()
 
 console.log(chalk.yellow(figlet.textSync('BBOT', { horizontalLayout: 'full' })))
-console.log(server);
