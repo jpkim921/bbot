605fb6c86f9e41cef5b223ede7dacf5ef90d1d2d
Jayon Kim <jpkim921@gmail.com>
2021-12-15 10:41:18 -0500

    grab llist of NFTS and parse results into JSON

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ import chalk from 'chalk'
 import clear from 'clear'
 import figlet from 'figlet'
 
-
+import {getListOfNFtsForSale} from './lib/listOfNFTs.js';
 clear()
 
 console.log(chalk.yellow(figlet.textSync('BBOT', { horizontalLayout: 'full' })))
+
+async function main() {
+    console.log(await getListOfNFtsForSale());
+}
+
+
+main()
