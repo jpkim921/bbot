a2d8d732cee3a34a6e5f2e15fc6378005f46351a
Jayon Kim <jpkim921@gmail.com>
2021-12-30 14:48:59 -0500

    add package-lock.json to gitignore

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ import chalk from 'chalk'
 import clear from 'clear'
 import figlet from 'figlet'
 
-import {getListOfNFtsForSale} from './lib/listOfNFTs.js';
+import {getListOfNFtsForSale, sortBy} from './lib/listOfNFTs.js';
 clear()
 
 console.log(chalk.yellow(figlet.textSync('BBOT', { horizontalLayout: 'full' })))
 
 async function main() {
-    console.log(await getListOfNFtsForSale());
+    const NFTs = await getListOfNFtsForSale();
+    console.log(NFTs);
+    await sortBy("Views");
 }
 
 
